32ccf1a1486f46e6557d6be3c324c709fe45692b
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2025-03-25 20:06:14 +0500

    Nest.js-7.3

---
+++ src/modules/module/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { UserService } from "../service/user.service";
+import { UserController } from "../controller/user.controller";
+
+@Module({
+    imports: [],
+    exports: [],
+    controllers: [UserController],
+    providers: [UserService]
+})
+export class UserModule { }
