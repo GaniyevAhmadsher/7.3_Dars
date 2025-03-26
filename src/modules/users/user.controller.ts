a16791aac8d8d9210b1a37d51925fb645797947d
GanievAhmadsher <ganievahmadsher6205@gmail.com>
2025-03-26 20:38:56 +0500

    Nest.js-7.3

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from "@nestjs/common";
-import { UserService } from "./user.service";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { IUser, UserService } from "./user.service";
 
-@Controller()
+@Controller("/users")
 export class UserController {
     constructor(private readonly userService: UserService) { }
+
+    @Post()
+    addOneUser(@Body() body: IUser): IUser {
+        return this.userService.insert(body)
+    }
+
+    @Get()
+    getAllUsers(): IUser[] {
+        return this.userService.find()
+    }
+
+    @Get()
+    getUserById(@Param("id") id: string): object {
+        return this.userService.findOne(+id)
+    }
 }
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
+export type IUser = { id?: number, name: string, email: string, password: string, age: number }
 
 @Injectable()
-export class UserService { }
+export class UserService {
+    #users: IUser[] = []
+
+    insert(body: IUser): IUser {
+        const data: IUser = { id: this.#users.length + 1, ...body }
+        this.#users.push(data)
+        return data
+    }
+
+    find(): IUser[] {
+        return this.#users
+    }
+
+    findOne(id: number): object {
+        const userOne = this.#users.find((v) => v.id === id)
+        if (userOne) return userOne
+        else throw new HttpException("Not Found", 404)
+    }
+}
